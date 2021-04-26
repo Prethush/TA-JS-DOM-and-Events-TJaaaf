35b870f370af532c4af0a76ba950fd07c10bcf74
prethush <ktmirash49@gmail.com>
2021-04-26 20:21:45 +0530

    form validation

--- block-BJaaes/code/script.js
+++ block-BJaaes/code/script.js
@@ -1,110 +1,95 @@
 let form = document.querySelector("form");
 
-function userName(userElem) {
+let userInfo = {};
+let errorMessages = {};
 
-    let userValue = userElem.value;
-    let userError = userElem.nextElementSibling;
-    console.log(userError);
-    if(userValue.length < 4) {
-        userError.innerText = `UserName can't be less than 4 characters`;
-        userElem.parentElement.classList.add("error");
-        return false;
-    } else {
-        userError.innerText = "";
-        userElem.parentElement.classList.add("success");
-        userElem.parentElement.classList.remove("error");
-        return true;
-    }
-}
 
-function fullName(nameElem){
-    let userError = nameElem.nextElementSibling;
-    if(nameElem.value.split("").some(e => Number(e))) {
-        userError.innerText = `Name can't be numbers`;
-        nameElem.parentElement.classList.add("error");
-        return false;
-    } 
-      else {
-         userError.innerText = "";
-         nameElem.parentElement.classList.add("success");
-         nameElem.parentElement.classList.remove("error");
-         return true;
-    }
-}
-
-function email(userEmail) {
-    let userError = userEmail.nextElementSibling;
-    if(!userEmail.value.includes("@") || userEmail.value.length < 6) {
-        userError.innerText = `Not a Valid Email`;
-        userEmail.parentElement.classList.add("error");
-        return false;
-        
-    } else {
-        userError.innerText = "";
-        userEmail.parentElement.classList.add("success");
-        userEmail.parentElement.classList.remove("error");
-        return true;
-   }
-    
-}
-
-function passwordValidatiion(password, confirmPassword){
-    let userError = confirmPassword.nextElementSibling;
-
-    if(password.value !== confirmPassword.value) {
-        userError.innerText = `Enter the correct password`;
-        console.log(userError);
-        confirmPassword.parentElement.classList.add("error");
-        password.parentElement.classList.add("error");
-        return false;
-    } else {
-        userError.innerText = "";
-        confirmPassword.parentElement.classList.add("success");
-        confirmPassword.parentElement.classList.remove("error")
-        password.parentElement.classList.add("success");
-        password.parentElement.classList.remove("error");
-        return true;
-   }
+function displayError(name) {
+    let elm = form.elements[name]
+    elm.nextElementSibling.innerText = errorMessages[name];  
+    elm.parentElement.classList.add("error");
+    return false;
 
 }
 
-function phone(userPhone) {
-    let userError = userPhone.nextElementSibling;
-    if(!userPhone.value.split("").every(e => Number(e))) {
-        userError.innerText = `Phone numbers can only be a number`;
-        userPhone.parentElement.classList.add("error");
-        return false;
-    } else if(userPhone.value.length < 7) {
-        userError.innerText = `Length of phone number can't be less than 7`;
-        userPhone.parentElement.classList.add("error");
-        return false;
-    }  else {
-        userError.innerText = "";
-        userPhone.parentElement.classList.add("success");
-        return true;
-   }
+function displaySuccess(name) {
+    let elm = form.elements[name]
+    elm.nextElementSibling.innerText = "";
+    errorMessages[name] = "";
+    elm.parentElement.classList.remove("error");        
+    elm.parentElement.classList.add("success");
+   return true;
+
 }
+
 function handleSubmit(event) {
     event.preventDefault();
-
-    let userElem = event.target.elements.username;
-    let nameElem = event.target.elements.name;
-    let userEmail = event.target.elements.email;
-    let userPhone = event.target.elements.tel;
-    let password = event.target.elements.passwordOne;
-    let confirmPassword =  event.target.elements.passwordTwo;
     
-   let value1 =  userName(userElem);
-   let value2 = fullName(nameElem);
-    let value3 = email(userEmail);
-    let value4 = phone(userPhone);
-    let value5 = passwordValidatiion(password, confirmPassword);
+    let elements = event.target.elements;
+    const username = elements.username.value;
+    const name = elements.name.value;
+    const email = elements.email.value;
+    const tel = elements.tel.value;
+    const passwordOne = elements.passwordOne.value;
+    const passwordTwo = elements.passwordTwo.value;
+    let val1, val2, val3, val4, val5;
+      
+    // console.log({username, name, email, phone, password, passwordCheck});
 
-    if(value1 && value2 && value3 && value4 && value5) {
-        alert(`User Added Successfully!`);
+    //Username
+    if(username.length <= 4) {
+        errorMessages.username = `Username can't be less than 4`;
+       val1 =  displayError("username");
+    } else {
+        val1 = displaySuccess("username"); 
     }
-        
 
+    //name 
+    if(name.split("").some(e => Number(e))) {
+        errorMessages.name = `Name can't be numbers`;
+       val2 =  displayError("name");
+    }else {
+       val2 =  displaySuccess("name");
+    }
+
+    //Email
+
+    if(!email.includes("@")) {
+        errorMessages.email = `Email must contain @`;
+       val3 =  displayError("email");
+    } else if(email.length < 6) {
+        errorMessages.email = `Email must be atleast 6 characters`;
+        val3 = displayError("email");
+    } else {
+        val3 = displaySuccess("email");
+    }
+
+    //Phone
+
+    if(!tel.split("").every(e => Number(e))) {
+        errorMessages.tel = `Phone numbers can only be a number`;
+       val4 =  displayError("tel");
+    } else if(tel.length < 7) {
+        errorMessages.tel = `Length of the phone number can't be less than 7`;
+        val4 = displayError("tel");
+    } else {
+        val4 = displaySuccess("tel");
+    }
+
+    //Password 
+
+    if(passwordOne !== passwordTwo) {
+        errorMessages.passwordTwo = `Password and Confirm password must be same`;
+        errorMessages.passwordOne = `Password and Confirm password must be same`;
+        val5 = displayError("passwordOne");
+        val5 = displayError("passwordTwo");
+    } else {
+        val5 = displaySuccess("passwordOne");
+        val5 = displaySuccess("passwordTwo");
+    }
+    if(val1 && val2 && val3 && val4 && val5) {
+        alert("User Added Successfully!");
+    }
 }
 
 
