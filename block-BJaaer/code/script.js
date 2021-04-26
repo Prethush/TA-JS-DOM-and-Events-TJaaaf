ff6b92a914d7768eb4f38e44f3a382474ad3cf36
prethush <ktmirash49@gmail.com>
2021-04-26 15:33:11 +0530

    made changes

--- block-BJaaer/code/script.js
+++ block-BJaaer/code/script.js
@@ -1,23 +1,26 @@
-let div = document.createElement("div");
-div.className = "small-container";
-let nameElement = document.createElement("h2");
-let emailElement = document.createElement("p");
-let moviesElement = document.createElement("p");
-let colorElement = document.createElement("p");
-let ratingElement = document.createElement("p");
-let bookElement = document.createElement("p");
-let terms = document.createElement("p");
+
 
 let form = document.querySelector(".container");
-let close = document.createElement("a");
-close.className = "close";
-close.innerText = "Close";
-div.append(close);
+
 
 
 console.log(close);
 function handleSubmit(event) {
     event.preventDefault();
+    let div = document.createElement("div");
+div.className = "small-container";
+let close = document.createElement("a");
+close.className = "close";
+close.innerText = "Close";
+div.append(close);
+    let nameElement = document.createElement("h2");
+    let emailElement = document.createElement("p");
+    let moviesElement = document.createElement("p");
+    let colorElement = document.createElement("p");
+    let ratingElement = document.createElement("p");
+    let bookElement = document.createElement("p");
+    let terms = document.createElement("p");
+
     nameElement.innerText = `Hello ${event.target.elements.name.value}`;
     emailElement.innerText = `Email: ${event.target.elements.email[0].value}`;
     moviesElement.innerText = `You Love: ${event.target.elements.gender.value}`;
@@ -31,7 +34,7 @@ function handleSubmit(event) {
      document.querySelector(".close").addEventListener("click", (event) => {
          event.preventDefault();
             div.style.display = "none";
-            location.reload();
+            // location.reload();
     }); 
     
     console.log(nameElement, emailElement, moviesElement, colorElement, ratingElement, bookElement, terms);
