414b4793fb88ee47ef97aa12a102cf4cd3079614
prethush <ktmirash49@gmail.com>
2021-04-27 13:42:15 +0530

    watch list

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -2,24 +2,63 @@
 let form = document.querySelector("form");
 let movieList = document.querySelector(".list");
 
-function handleClick(event) {
-    let newText =  event.target.previousElementSibling.innerHTML.strike();
-     event.target.previousElementSibling.innerHTML= newText;
+function handleDelete(event) {
+    console.dir(event.target);
+    let id = event.target.dataset.id;
+    allMovies.splice(id, 1);
+    createmovieUI();
 }
+
+let allMovies = [
+    {
+        name: "Inception",
+        watched: false
+    },
+
+    {
+        name: "Forest Gump",
+        watched: true
+    }
+];
 
 function handleSubmit(event) {
     event.preventDefault();
-    let elem = event.target.elements;
-    let div = document.createElement("div");
-    div.classList.add("flex","jcb", "aic", "movieDetails");
-    let h2 = document.createElement("h2");
-    h2.innerText = elem.movie.value;
-    let button = document.createElement("button");
-    button.classList.add("cross");
-    button.innerText  = "X";
-    div.append(h2, button);
-    movieList.append(div);
-    button.addEventListener("click", handleClick);
+    allMovies.push({
+        name: event.target.movie.value,
+        watched: false
+    });
+    event.target.movie.value = "";
+    createmovieUI();
+    
 }
 
+function handleChange(event) {
+    let id = event.target.id;
+    allMovies[id].watched = !allMovies[id].watched;
+}
+
+function createmovieUI() {
+    movieList.innerHTML = "";
+    allMovies.forEach((movie, i) => {
+        let div = document.createElement("div");
+        div.classList.add("flex", "aic", "movieDetails");
+        let input = document.createElement("input");
+        input.type = "checkbox";
+        input.id = i;
+        input.classList.add("checkbox");
+        input.checked = movie.watched;
+        let h2 = document.createElement("h2");
+        h2.innerText = movie.name;
+        let button = document.createElement("button");
+        button.classList.add("cross");
+        button.innerText  = "X";
+        button.setAttribute("data-id", i);
+        div.append(input, h2, button);
+        movieList.append(div);
+        button.addEventListener("click", handleDelete);
+        input.addEventListener("change", handleChange);
+    })
+}
 form.addEventListener("submit", handleSubmit);
+
+createmovieUI();
